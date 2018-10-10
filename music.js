cd85f91f30d094b0fdf8fbcdf093d08df0f0584a
Vizmo FIlms <Vizmo@Vizmos-MacBook-Pro.local>
2018-10-10 16:00:09 -0400

    Portfolio

---
+++ music.js
@@ -0,0 +1,8 @@
+// var audio = new Audio('./assets/Beneath the Mask.mp3');
+// audio.play();
+
+// var audio = new Audio('./assets/Rain.mp3');
+// audio.play();
+
+<audio id="audio" src="./assets/Beneath the Mask.mp3"></audio>
+<a onClick="document.getElementById('audio').play()">Click here to hear.</a>
